bb72e4f3e8990ae0e44bd31853618344930924d6
engineerrajrana-oss <engineer.raj.rana@gmail.com>
2025-10-05 19:13:08 +0530

    Day1_Q1
    Done

---
+++ q1.c
@@ -0,0 +1,14 @@
+//Write a program to input two numbers and display their sum.
+#include <stdio.h> 
+int main () {
+    int num1,num2;
+    printf("enter the number1");
+    scanf("%d" , & num1);
+     printf("enter the number2");
+    scanf("%d",  & num2);
+    printf("the sum of %d and %d are %d", & num1 + num2) ;
+    return 0;
+    
+
+}
+
